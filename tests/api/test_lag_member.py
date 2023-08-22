70ed40acc90afc759b7a9bd747c4bcfb316eeed3
selldinesh <dinesh.sellappan@keysight.com>
2023-08-22 22:00:05 +0000

    adding lag cleanup

--- tests/api/test_lag_member.py
+++ tests/api/test_lag_member.py
@@ -42,34 +42,9 @@ class TestSaiLagMember:
 
     def test_lag_member_remove(self, npu):
         commands = [
-            {
-                'name': 'lag_1',
-                'op': 'create',
-                'type': 'SAI_OBJECT_TYPE_LAG',
-                'attributes': [],
-            },
-            {
-                'name': 'port_1',
-                'op': 'create',
-                'type': 'SAI_OBJECT_TYPE_PORT',
-                'attributes': [
-                    'SAI_PORT_ATTR_HW_LANE_LIST',
-                    '2:10,11',
-                    'SAI_PORT_ATTR_SPEED',
-                    '10',
-                ],
-            },
-            {
-                'name': 'lag_member_1',
-                'op': 'create',
-                'type': 'SAI_OBJECT_TYPE_LAG_MEMBER',
-                'attributes': [
-                    'SAI_LAG_MEMBER_ATTR_LAG_ID',
-                    '$lag_1',
-                    'SAI_LAG_MEMBER_ATTR_PORT_ID',
-                    '$port_1',
-                ],
-            },
+            {'name': 'lag_member_1', 'op': 'remove'},
+            {'name': 'port_1', 'op': 'remove'},
+            {'name': 'lag_1', 'op': 'remove'},
         ]
 
         results = [*npu.process_commands(commands, cleanup=True)]
