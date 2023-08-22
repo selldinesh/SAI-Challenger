7eb75bb615727ffcf5ecc9e14ae435a1919d3dc2
selldinesh <dinesh.sellappan@keysight.com>
2023-08-22 21:46:18 +0000

    adding lag cleanup

--- tests/api/test_lag_member.py
+++ tests/api/test_lag_member.py
@@ -43,12 +43,37 @@ class TestSaiLagMember:
 
     def test_lag_member_remove(self, npu):
         commands = [
-            {'name': 'lag_member_1', 'op': 'remove'},
-            {'name': 'port_1', 'op': 'remove'},
-            {'name': 'lag_1', 'op': 'remove'},
+            {
+                'name': 'lag_1',
+                'op': 'create',
+                'type': 'SAI_OBJECT_TYPE_LAG',
+                'attributes': [],
+            },
+            {
+                'name': 'port_1',
+                'op': 'create',
+                'type': 'SAI_OBJECT_TYPE_PORT',
+                'attributes': [
+                    'SAI_PORT_ATTR_HW_LANE_LIST',
+                    '2:10,11',
+                    'SAI_PORT_ATTR_SPEED',
+                    '10',
+                ],
+            },
+            {
+                'name': 'lag_member_1',
+                'op': 'create',
+                'type': 'SAI_OBJECT_TYPE_LAG_MEMBER',
+                'attributes': [
+                    'SAI_LAG_MEMBER_ATTR_LAG_ID',
+                    '$lag_1',
+                    'SAI_LAG_MEMBER_ATTR_PORT_ID',
+                    '$port_1',
+                ],
+            },
         ]
 
-        results = [*npu.process_commands(commands)]
+        results = [*npu.process_commands(commands, cleanup=True)]
         print('======= SAI commands RETURN values remove =======')
         pprint(results)
         assert all(
