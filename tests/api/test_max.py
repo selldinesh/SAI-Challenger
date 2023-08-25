c21b640e78b267f44b5d3940d854a41f6faedd71
selldinesh <dinesh.sellappan@keysight.com>
2023-08-25 23:58:16 +0000

    max

---
+++ tests/api/test_max.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+
+
+class TestSaiMax:
+    # object with no attributes
+
+    def test_max_create(self, npu):
+        commands = [
+            {
+                'name': 'max_1',
+                'op': 'create',
+                'type': 'SAI_OBJECT_TYPE_MAX',
+                'attributes': [],
+            }
+        ]
+
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values create =======')
+        pprint(results)
+        assert all(results), 'Create error'
+
+    def test_max_remove(self, npu):
+        commands = [{'name': 'max_1', 'op': 'remove'}]
+
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values remove =======')
+        pprint(results)
+        assert all(
+            [result == 'SAI_STATUS_SUCCESS' for result in results]
+        ), 'Remove error'
