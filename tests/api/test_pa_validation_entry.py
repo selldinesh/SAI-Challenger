ca2feb679968e5871763e5f22b3ca4029baacb54
selldinesh <dinesh.sellappan@keysight.com>
2023-08-28 16:53:28 +0000

    pa

--- tests/api/test_pa_validation_entry.py
+++ tests/api/test_pa_validation_entry.py
@@ -1,21 +1,17 @@
 from pprint import pprint
 
 
-class TestSaiOutboundRoutingEntry:
+class TestSaiPaValidationEntry:
     # object with no attributes
 
-    def test_outbound_routing_entry_create(self, npu):
+    def test_pa_validation_entry_create(self, npu):
         commands = [
             {
-                'name': 'outbound_routing_entry_1',
+                'name': 'pa_validation_entry_1',
                 'op': 'create',
-                'type': 'SAI_OBJECT_TYPE_OUTBOUND_ROUTING_ENTRY',
+                'type': 'SAI_OBJECT_TYPE_PA_VALIDATION_ENTRY',
                 'attributes': [],
-                'key': {
-                    'switch_id': '$SWITCH_ID',
-                    'eni_id': 'TODO',
-                    'destination': 'TODO',
-                },
+                'key': {'switch_id': '$SWITCH_ID', 'vnet_id': 'TODO', 'sip': 'TODO'},
             }
         ]
 
@@ -24,15 +20,11 @@ class TestSaiOutboundRoutingEntry:
         pprint(results)
         assert all(results), 'Create error'
 
-    def test_outbound_routing_entry_remove(self, npu):
+    def test_pa_validation_entry_remove(self, npu):
         commands = [
             {
-                'name': 'outbound_routing_entry_1',
-                'key': {
-                    'switch_id': '$SWITCH_ID',
-                    'eni_id': 'TODO',
-                    'destination': 'TODO',
-                },
+                'name': 'pa_validation_entry_1',
+                'key': {'switch_id': '$SWITCH_ID', 'vnet_id': 'TODO', 'sip': 'TODO'},
                 'op': 'remove',
             }
         ]
