ac1dd8d3d4c2e58d22f7dc18e3b473c2a1b86d9f
selldinesh <dinesh.sellappan@keysight.com>
2023-08-28 04:32:15 +0000

    null

---
+++ tests/api/test_null.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+
+
+class TestSaiNull:
+    # object with no attributes
+
+    def test_null_create(self, npu):
+        commands = [
+            {
+                'name': 'null_1',
+                'op': 'create',
+                'type': 'SAI_OBJECT_TYPE_NULL',
+                'attributes': [],
+            }
+        ]
+
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values create =======')
+        pprint(results)
+        assert all(results), 'Create error'
+
+    def test_null_remove(self, npu):
+        commands = [{'name': 'null_1', 'op': 'remove'}]
+
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values remove =======')
+        pprint(results)
+        assert all(
+            [result == 'SAI_STATUS_SUCCESS' for result in results]
+        ), 'Remove error'
