d32a6f957d4296f08545aa8e31a7e512c4ce3f78
selldinesh <dinesh.sellappan@keysight.com>
2023-09-11 16:07:30 +0000

    table group, table group memeber Run1

--- tests/api/test_acl_table_group.py
+++ tests/api/test_acl_table_group.py
@@ -1,16 +1,19 @@
 from pprint import pprint
 
 
-class TestSaiAclTable:
+class TestSaiAclTableGroup:
     # object with no parents
 
-    def test_acl_table_create(self, npu):
+    def test_acl_table_group_create(self, npu):
         commands = [
             {
-                'name': 'acl_table_1',
+                'name': 'acl_table_group_1',
                 'op': 'create',
-                'type': 'SAI_OBJECT_TYPE_ACL_TABLE',
-                'attributes': ['SAI_ACL_TABLE_ATTR_ACL_STAGE', 'SAI_ACL_STAGE_INGRESS'],
+                'type': 'SAI_OBJECT_TYPE_ACL_TABLE_GROUP',
+                'attributes': [
+                    'SAI_ACL_TABLE_GROUP_ATTR_ACL_STAGE',
+                    'SAI_ACL_STAGE_INGRESS',
+                ],
             }
         ]
 
@@ -18,12 +21,12 @@ class TestSaiAclTable:
         print('======= SAI commands RETURN values create =======')
         pprint(results)
 
-    def test_sai_acl_table_attr_entry_list_get(self, npu):
+    def test_sai_acl_table_group_attr_member_list_get(self, npu):
         commands = [
             {
-                'name': 'acl_table_1',
+                'name': 'acl_table_group_1',
                 'op': 'get',
-                'attributes': ['SAI_ACL_TABLE_ATTR_ENTRY_LIST'],
+                'attributes': ['SAI_ACL_TABLE_GROUP_ATTR_MEMBER_LIST'],
             }
         ]
         results = [*npu.process_commands(commands)]
@@ -35,12 +38,12 @@ class TestSaiAclTable:
         print(r_value)
         assert r_value == 'TODO', 'Get error, expected TODO but got %s' % r_value
 
-    def test_sai_acl_table_attr_available_acl_entry_get(self, npu):
+    def test_sai_acl_table_group_attr_chain_group_list_get(self, npu):
         commands = [
             {
-                'name': 'acl_table_1',
+                'name': 'acl_table_group_1',
                 'op': 'get',
-                'attributes': ['SAI_ACL_TABLE_ATTR_AVAILABLE_ACL_ENTRY'],
+                'attributes': ['SAI_ACL_TABLE_GROUP_ATTR_CHAIN_GROUP_LIST'],
             }
         ]
         results = [*npu.process_commands(commands)]
@@ -52,25 +55,8 @@ class TestSaiAclTable:
         print(r_value)
         assert r_value == 'TODO', 'Get error, expected TODO but got %s' % r_value
 
-    def test_sai_acl_table_attr_available_acl_counter_get(self, npu):
-        commands = [
-            {
-                'name': 'acl_table_1',
-                'op': 'get',
-                'attributes': ['SAI_ACL_TABLE_ATTR_AVAILABLE_ACL_COUNTER'],
-            }
-        ]
-        results = [*npu.process_commands(commands)]
-        print('======= SAI commands RETURN values get =======')
-        for command in results:
-            for attribute in command:
-                pprint(attribute.raw())
-        r_value = results[0][0].value()
-        print(r_value)
-        assert r_value == 'TODO', 'Get error, expected TODO but got %s' % r_value
-
-    def test_acl_table_remove(self, npu):
-        commands = [{'name': 'acl_table_1', 'op': 'remove'}]
+    def test_acl_table_group_remove(self, npu):
+        commands = [{'name': 'acl_table_group_1', 'op': 'remove'}]
 
         results = [*npu.process_commands(commands)]
         print('======= SAI commands RETURN values remove =======')
