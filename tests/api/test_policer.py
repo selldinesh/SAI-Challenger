f239d454abcae8a7dec425aab1c9a4103a642cbf
selldinesh <dinesh.sellappan@keysight.com>
2023-09-11 15:50:47 +0000

    adding passed cases for get set

--- tests/api/test_policer.py
+++ tests/api/test_policer.py
@@ -1,5 +1,7 @@
 from pprint import pprint
 
+import pytest
+
 
 class TestSaiPolicer:
     # object with no parents
@@ -23,13 +25,225 @@ class TestSaiPolicer:
         print('======= SAI commands RETURN values create =======')
         pprint(results)
 
-    def test_policer_remove(self, npu):
+    @pytest.mark.dependency(name='test_sai_policer_attr_cbs_set')
+    def test_sai_policer_attr_cbs_set(self, npu):
         commands = [
             {
                 'name': 'policer_1',
-                'op': 'remove',
+                'op': 'set',
+                'attributes': ['SAI_POLICER_ATTR_CBS', '0'],
             }
         ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_cbs_set'])
+    def test_sai_policer_attr_cbs_get(self, npu):
+        commands = [
+            {'name': 'policer_1', 'op': 'get', 'attributes': ['SAI_POLICER_ATTR_CBS']}
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == '0', 'Get error, expected 0 but got %s' % r_value
+
+    @pytest.mark.dependency(name='test_sai_policer_attr_cir_set')
+    def test_sai_policer_attr_cir_set(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'set',
+                'attributes': ['SAI_POLICER_ATTR_CIR', '0'],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_cir_set'])
+    def test_sai_policer_attr_cir_get(self, npu):
+        commands = [
+            {'name': 'policer_1', 'op': 'get', 'attributes': ['SAI_POLICER_ATTR_CIR']}
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == '0', 'Get error, expected 0 but got %s' % r_value
+
+    @pytest.mark.dependency(name='test_sai_policer_attr_pbs_set')
+    def test_sai_policer_attr_pbs_set(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'set',
+                'attributes': ['SAI_POLICER_ATTR_PBS', '0'],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_pbs_set'])
+    def test_sai_policer_attr_pbs_get(self, npu):
+        commands = [
+            {'name': 'policer_1', 'op': 'get', 'attributes': ['SAI_POLICER_ATTR_PBS']}
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == '0', 'Get error, expected 0 but got %s' % r_value
+
+    @pytest.mark.dependency(name='test_sai_policer_attr_pir_set')
+    def test_sai_policer_attr_pir_set(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'set',
+                'attributes': ['SAI_POLICER_ATTR_PIR', '0'],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_pir_set'])
+    def test_sai_policer_attr_pir_get(self, npu):
+        commands = [
+            {'name': 'policer_1', 'op': 'get', 'attributes': ['SAI_POLICER_ATTR_PIR']}
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == '0', 'Get error, expected 0 but got %s' % r_value
+
+    @pytest.mark.dependency(name='test_sai_policer_attr_green_packet_action_set')
+    def test_sai_policer_attr_green_packet_action_set(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'set',
+                'attributes': [
+                    'SAI_POLICER_ATTR_GREEN_PACKET_ACTION',
+                    'SAI_PACKET_ACTION_FORWARD',
+                ],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_green_packet_action_set'])
+    def test_sai_policer_attr_green_packet_action_get(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'get',
+                'attributes': ['SAI_POLICER_ATTR_GREEN_PACKET_ACTION'],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == 'SAI_PACKET_ACTION_FORWARD', (
+            'Get error, expected SAI_PACKET_ACTION_FORWARD but got %s' % r_value
+        )
+
+    @pytest.mark.dependency(name='test_sai_policer_attr_yellow_packet_action_set')
+    def test_sai_policer_attr_yellow_packet_action_set(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'set',
+                'attributes': [
+                    'SAI_POLICER_ATTR_YELLOW_PACKET_ACTION',
+                    'SAI_PACKET_ACTION_FORWARD',
+                ],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_yellow_packet_action_set'])
+    def test_sai_policer_attr_yellow_packet_action_get(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'get',
+                'attributes': ['SAI_POLICER_ATTR_YELLOW_PACKET_ACTION'],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == 'SAI_PACKET_ACTION_FORWARD', (
+            'Get error, expected SAI_PACKET_ACTION_FORWARD but got %s' % r_value
+        )
+
+    @pytest.mark.dependency(name='test_sai_policer_attr_red_packet_action_set')
+    def test_sai_policer_attr_red_packet_action_set(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'set',
+                'attributes': [
+                    'SAI_POLICER_ATTR_RED_PACKET_ACTION',
+                    'SAI_PACKET_ACTION_FORWARD',
+                ],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        pprint(results)
+
+    @pytest.mark.dependency(depends=['test_sai_policer_attr_red_packet_action_set'])
+    def test_sai_policer_attr_red_packet_action_get(self, npu):
+        commands = [
+            {
+                'name': 'policer_1',
+                'op': 'get',
+                'attributes': ['SAI_POLICER_ATTR_RED_PACKET_ACTION'],
+            }
+        ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values get =======')
+        for command in results:
+            for attribute in command:
+                pprint(attribute.raw())
+        r_value = results[0][0].value()
+        print(r_value)
+        assert r_value == 'SAI_PACKET_ACTION_FORWARD', (
+            'Get error, expected SAI_PACKET_ACTION_FORWARD but got %s' % r_value
+        )
+
+
+    def test_policer_remove(self, npu):
+        commands = [{'name': 'policer_1', 'op': 'remove'}]
 
         results = [*npu.process_commands(commands)]
         print('======= SAI commands RETURN values remove =======')
