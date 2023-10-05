a33f10455f338798cadca61928280e66aff90485
selldinesh <dinesh.sellappan@keysight.com>
2023-10-05 15:34:59 +0000

    adding additional tofino vs pass cases

--- tests/api/test_next_hop_group.py
+++ tests/api/test_next_hop_group.py
@@ -1,6 +1,16 @@
 from pprint import pprint
 
+import pytest
 
+
+@pytest.fixture(scope='module', autouse=True)
+def skip_all(testbed_instance):
+    testbed = testbed_instance
+    if testbed is not None and len(testbed.npu) != 1:
+        pytest.skip('invalid for {} testbed'.format(testbed.name))
+
+
+@pytest.mark.npu
 class TestSaiNextHopGroup:
     # object with no parents
 
@@ -21,13 +31,23 @@ class TestSaiNextHopGroup:
         print('======= SAI commands RETURN values create =======')
         pprint(results)
 
-    def test_next_hop_group_remove(self, npu):
+
+    @pytest.mark.dependency(name='test_sai_next_hop_group_attr_set_switchover_set')
+    def test_sai_next_hop_group_attr_set_switchover_set(self, npu):
         commands = [
             {
                 'name': 'next_hop_group_1',
-                'op': 'remove',
+                'op': 'set',
+                'attributes': ['SAI_NEXT_HOP_GROUP_ATTR_SET_SWITCHOVER', 'false'],
             }
         ]
+        results = [*npu.process_commands(commands)]
+        print('======= SAI commands RETURN values set =======')
+        pprint(results)
+
+
+    def test_next_hop_group_remove(self, npu):
+        commands = [{'name': 'next_hop_group_1', 'op': 'remove'}]
 
         results = [*npu.process_commands(commands)]
         print('======= SAI commands RETURN values remove =======')
